f3604b33f6939401854b8140bb666f9e902b78eb
eykamt <embergameslp@gmail.com>
2023-04-25 21:30:14 -0400

    created admin and started home. Added deleted number in admin

--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -1,6 +1,7 @@
 import { createWebHistory, createRouter } from 'vue-router'
 import Home from '../views/HomeView.vue'
 import About from '../views/AboutView.vue'
+import Admin from '../views/AdminView.vue'
 
 const routes = [
   {
@@ -12,7 +13,12 @@ const routes = [
     path: '/about',
     name: 'about',
     component: About
-  }
+  },
+  {
+    path: '/admin',
+    name: 'admin',
+    component: Admin
+  },
 ]
 
 const router = createRouter({
--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,119 +1,40 @@
 <template>
   <div class="d-flex flex-column justify-center align-center">
-    <h1 class="mt-4">Make A Post</h1>
-    <v-form
-      class="mb-10"
-      style="width: 90%; max-width: 400px;"
-      @submit.prevent="uploadPost"
+    <div
+      v-for="post in posts"
+      :key="post._id"
+      class="post flex-wrap flex-column justify-center align-center"
     >
-      <v-text-field
-        v-model="addPost.title"
-        prepend-icon="mdi-format-title"
-        label="title"
-      />
-      <v-text-field
-        v-model="addPost.body"
-        prepend-icon="mdi-text"
-        label="body"
-      />
-      <v-file-input
-        v-model="addPost.images"
-        @change="encodeImage"
-        multiple
-        label="image"
-        prepend-icon="mdi-camera"
-        accept="image/*"
-      ></v-file-input>
-      <img 
-        v-for="image in addPost.imageEncodings"
-        :key="image"
-        :src="image"
-        class="thumbnail"
-        alt="image"
-      />
-      <v-btn
-        :disabled="disablePostButton"
-        color="primary" 
-        type="submit"
-      >add post</v-btn>
-    </v-form>
-    <div 
-      style="width: 95%; height: 600px; overflow-y: scroll; border-top: 1px solid black;"
-      class="d-flex flex-wrap flex-start justify-center align-start"
-    >
-      <div
-        v-for="post in posts"
-        :key="post._id"
-        class="post flex-wrap flex-column justify-center align-center"
-      >
-        <PostDisplay
-          :post="post"
-          @delete="deletePost(post._id)"
-        />
-      </div>
-      <div v-if="loadingPosts">
-        <v-progress-circular
-          indeterminate
-          color="primary"
-        ></v-progress-circular>
-      </div>
-      <div 
-        v-else-if="posts.length === 0"
-        style="color: red;"
-      >
-        <h2>
-          No posts yet
-        </h2>
-      </div>
+    <img
+      :src="post.images[0]"
+      class="thumbnail"
+      alt="Cannot display image"
+    />
     </div>
-    <v-btn
-      v-if="posts.length"
-      @click="deletePosts"
-      class="my-10"
-      color="error"
-    >delete all posts</v-btn>
+    <div v-if="loadingPosts">
+      <v-progress-circular
+        indeterminate
+        color="primary"
+      ></v-progress-circular>
+    </div>
+    <div 
+      v-else-if="posts.length === 0"
+      style="color: red;"
+    >
+      <h2>
+        No posts yet
+      </h2>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
 import axios from "axios";
-import Compress from "compress.js";
-import PostDisplay from "../components/PostDisplay.vue";
 import { ref, watch, computed } from "vue";
 
 const posts = ref([]);
 const loadingPosts = ref(false);
 
-const addPost = ref({
-  title: "",
-  body: "",
-  images: [] as Blob[],
-  imageEncodings: [] as string[] | ArrayBuffer[],
-})
-
-const showBody = ref(false);
-
-const disablePostButton = computed(() => {
-  return !addPost.value.title
-})
-
-async function encodeImage() {
-  for (let i = 0; i < addPost.value.images.length; i++) {
-    const img = addPost.value.images[i];
-    if (!img) {
-      addPost.value.imageEncodings[i] = "";
-      continue;
-    }
-    if (typeof img === 'string') continue;
-    addPost.value.imageEncodings[i] = await compressBase64Image(img);
-  }
-}
-
-watch(() => addPost.value.images, (newVal) => {
-  if (newVal.length === 0) {
-    addPost.value.imageEncodings = [];
-  }
-})
 
 async function fetchPosts() {
   loadingPosts.value = true;
@@ -122,54 +43,8 @@ async function fetchPosts() {
   posts.value = data.reverse();
 }
 
-async function uploadPost() {
-  const newPost = {
-    title: addPost.value.title,
-    body: addPost.value.body,
-    images: addPost.value.imageEncodings,
-  };
-
-  const size = new TextEncoder().encode(JSON.stringify(newPost)).length
-  const kiloBytes = size / 1024;
-  const megaBytes = kiloBytes / 1024;
-  console.log(kiloBytes, "KB");
-
-  await axios.post("/api/posts", newPost);
-  posts.value.unshift(newPost);
-  addPost.value = {
-    title: "",
-    body: "",
-    images: [],
-    imageEncodings: [],
-  }
-}
-
 fetchPosts();
 
-async function deletePosts() {
-  await axios.delete("/api/posts");
-  posts.value = [];
-}
-
-async function deletePost(id: string) {
-  await axios.delete(`/api/posts/${id}`);
-  posts.value = posts.value.filter((post) => post._id !== id);
-}
-
-async function compressBase64Image(image) {
-  const compress = new Compress();
-  try {
-    const compressedImage = await compress.compress([image], {
-      size: .1, // the max size in MB, defaults to 2MB
-      quality: .1, // the quality of the image, max is 1,
-    })
-    return compressedImage.map((image) => {
-      return image.prefix + image.data;
-    })[0] as string;
-  } catch (error) {
-    console.warn(error)
-  }
-}
 </script>
 
 <style scoped>

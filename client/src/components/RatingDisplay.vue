4a6598da552a16a45ef85c5ce333de0b7ec58555
eykamt <embergameslp@gmail.com>
2023-05-16 18:54:37 -0400

    add fallback for no ratings

--- client/src/components/RatingDisplay.vue
+++ client/src/components/RatingDisplay.vue
@@ -1,35 +1,44 @@
 <template>
   <div>
-    <v-container class=" d-flex flex-row">
+    <v-row class=" d-flex flex-row">
       <v-col>
         <v-btn icon="mdi-thumb-up"></v-btn>
       </v-col>
-      <v-vol>
-        <v-tooltip activator="child"  :text="ratingValue + '% of people like this post.'">
-          <template v-slot:activator="on">
-            <v-progress-linear 
-              :color="ratingValue >= 50 ? 'success' : 'error'" 
-              :model-value="ratingValue"
-              v-on="on"
-              style="width: 10vw; "
-              class="mt-8"
-            >
-            </v-progress-linear>
-          </template>
-        </v-tooltip>
-      </v-vol>
+      <v-col>
+        <div v-if="ratingValue">  
+          <v-tooltip activator="child"  :text="ratingValue + '% of people like this post.'">
+            <template v-slot:activator="on">
+              <v-progress-linear
+                :color="ratingValue >= 50 ? 'success' : 'error'" 
+                :model-value="ratingValue"
+                v-on="on"
+                style="width: 10vw; "
+                class="mt-5"
+              >
+              </v-progress-linear>
+            </template>
+          </v-tooltip>
+        </div>
+        <v-sheet 
+          v-else
+          class="text-center text-sm-overline mt-3"
+          style="width: 10vw; "
+        >
+          No Ratings Yet
+        </v-sheet>
+      </v-col>
       <v-col>
         <v-btn icon="mdi-thumb-down"></v-btn>
       </v-col>
-    </v-container>
-    <div class="text-overline d-flex align-center justify-center">
-      <slot>Rating</slot>
-    </div>
+    </v-row>
+      <div class="text-overline d-flex align-center justify-center mb-6">
+        <slot>Rating</slot>
+      </div>
   </div>
 </template>
 
 <script setup lang="ts">
 import { ref } from "vue";
 
-const ratingValue = ref(50);
+const ratingValue = ref(89);
 </script>

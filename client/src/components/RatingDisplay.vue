db0dbdbf9db60dc0076c2f937ae8a7d14c782cd2
Thomas Eykamp <embergameslp@gmail.com>
2023-05-28 01:20:26 -0400

    fix like display bar bug

--- client/src/components/RatingDisplay.vue
+++ client/src/components/RatingDisplay.vue
@@ -9,7 +9,7 @@
         ></v-btn>
       </v-col>
       <v-col>
-        <div v-if="ratingValue">  
+        <div v-if="props.interactions.likes || props.interactions.dislikes || userLikeValue">  
         
               <v-progress-linear
                 :color="ratingValue >= 50 ? 'success' : 'error'" 
@@ -57,9 +57,8 @@ const props = defineProps<{
 
 const userLikeValue = ref(0);
 const ratingValue = computed(() => {
-  return (props.interactions.likes - props.interactions.dislikes + userLikeValue.value) / 
-         (props.interactions.likes + props.interactions.dislikes + userLikeValue.value ? 1 : 0) * 100
-
+  return (props.interactions.likes + userLikeValue.value) / (props.interactions.likes + props.interactions.dislikes + (userLikeValue.value ? 1 : 0)) * 100
+  // / (props.interactions.likes + props.interactions.dislikes + userLikeValue.value ? 1 : 0) * 100
 });
 
 function handleLike() {

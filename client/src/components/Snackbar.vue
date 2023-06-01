7381fda83ae7d62c851e577f5be53c148a4c59ff
eykamt <embergameslp@gmail.com>
2023-05-31 20:47:00 -0400

    added snackbar component

---
+++ client/src/components/Snackbar.vue
@@ -0,0 +1,20 @@
+<template>
+<div>
+  <v-snackbar
+    :timeout="timeout"
+    color="success"
+  >
+    <slot>success</slot>
+  </v-snackbar>
+</div>
+</template>
+
+<script setup lang="ts">
+import {ref} from 
+
+const props = defineProps<{
+  timeout: number,
+}>()
+
+
+</script>

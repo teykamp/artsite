43ea1ca83187f8cd314a53a2dcc67fe37cdd6f83
eykamt <embergameslp@gmail.com>
2023-06-21 12:08:36 -0400

    added tags to search filter

--- client/src/composables/useQueryFilter.ts
+++ client/src/composables/useQueryFilter.ts
@@ -4,7 +4,9 @@ import { computed } from 'vue'
 export function useQueryFilter(query: Ref<string>, items: Ref<any[]>) {
   const searchedItems = computed(() => {
     return items.value.filter((item) => {
-      return item.title.toLowerCase().includes(query.value.toLowerCase())
+      return item.tagData.some((tag) => tag.includes(query.value)) 
+      || (item.tagData.length === 0 && query.value === '')
+      || item.title.toLowerCase().includes(query.value.toLowerCase()) 
     })
   })
 

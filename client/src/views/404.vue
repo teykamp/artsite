784659749a913606e88bf5d669fbb4a03b9cbe63
Thomas Eykamp <embergameslp@gmail.com>
2023-06-27 14:01:33 -0400

    add navbar to 404

--- client/src/views/404.vue
+++ client/src/views/404.vue
@@ -1,5 +1,15 @@
 <template>
   <div class="d-flex flex-column justify-center align-center mt-10">
+    <NavBar 
+        v-model:handleNavDrawer="handleNavDrawer"
+    />
+
+    <NavDrawer 
+      :drawer="drawer"
+    />
+    <div class="my-6"></div>
+    <div class="my-6"></div>
+
     <Alert 
       type="info"
       title="404"
@@ -9,6 +19,14 @@
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue'
 import Alert from '../components/Alert.vue'
+import NavBar from '../components/NavBar.vue'
+import NavDrawer from '../components/NavDrawer.vue'
 
+const drawer = ref(false)
+
+function handleNavDrawer() {
+  drawer.value = !drawer.value
+}
 </script>

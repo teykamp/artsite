ec41d7aa3dbf0acfbebef950d6cb8fd5c42611eb
eykamt <embergameslp@gmail.com>
2023-07-29 16:21:21 -0400

    fix indentation

--- client/src/components/CommentDisplay.vue
+++ client/src/components/CommentDisplay.vue
@@ -1,21 +1,21 @@
 <template>
-	<div>
-		<v-card
-			elevation="0"
-			:subtitle="dateDisplay(date.toString())"
-			:text="body"
-			style="max-width: 72ch; white-space: pre-wrap;"
-		>
-		</v-card>
-	</div>
+<div>
+  <v-card
+    elevation="0"
+    :subtitle="dateDisplay(date.toString())"
+    :text="body"
+    style="max-width: 72ch; white-space: pre-wrap;"
+  >
+  </v-card>
+</div>
 </template>
 
 <script setup lang="ts">
 import { dateDisplay } from '../composables/dateDisplay';
 
 const props = defineProps<{
-	body: string,
-	date: string
+  body: string,
+  date: string
 }>()
 
 </script>

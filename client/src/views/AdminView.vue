1f767558bd6ebd0b21604172e6bbca00af343f80
eykamt <embergameslp@gmail.com>
2023-07-30 15:20:38 -0400

    remove comment structure

--- client/src/views/AdminView.vue
+++ client/src/views/AdminView.vue
@@ -166,10 +166,10 @@ async function uploadPost() {
     imageEncodings: [],
     tagData: [],
   }
-  // create empty comment section for posts
-  // posts dont have ID until pulled from DB!!!
-  await axios.post(`/api/comments/`, {postId: 123, comments: []})
-  // await axios.post(`/api/comments/${displayPosts.value[0]._id}`, {id: displayPosts.value[0]._id, comments: []})
+  // // create empty comment section for posts
+  // // posts dont have ID until pulled from DB!!!
+  // await axios.post(`/api/comments/`, {postId: 123, comments: []})
+  // // await axios.post(`/api/comments/${displayPosts.value[0]._id}`, {id: displayPosts.value[0]._id, comments: []})
 
 }
 

c6e63d9b3cdc26471b23ad6685611ecc524571c6
eykamt <embergameslp@gmail.com>
2023-08-06 18:20:36 -0400

    add dialog component

---
+++ client/src/components/Dialog.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <v-dialog
+      v-model="showDialog"
+      width="auto"
+      >
+      <v-card>
+        <div class="pt-8 px-8">
+          <slot name="content"></slot>
+        </div>
+        <v-card-actions class="d-flex justify-center">
+          <slot name="actions"></slot>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { computed } from 'vue'
+
+const props = defineProps<{
+  showDialog: boolean,
+}>()
+
+const emits = defineEmits([
+  "update:showDialog",
+])
+
+const showDialog = computed({
+  get: () => props.showDialog,
+  set: (value) => emits("update:showDialog", value)
+})
+</script>

e9e5db07a72c96121ded364c9ee3e92c763508c4
eykamt <embergameslp@gmail.com>
2023-08-08 23:01:32 -0400

    improve fetch post code

--- client/src/views/SpecificPost.vue
+++ client/src/views/SpecificPost.vue
@@ -6,15 +6,20 @@
 
 <script setup lang="ts">
 import axios from "axios";
-import { ref } from 'vue'
+import { ref, created } from 'vue'
 import { useRoute } from 'vue-router'
 import MainPostDisplay from '../components/MainPostDisplay.vue';
 import type { Post } from '../types'
 
 const post = ref<Post>()
-async function fetchPost(postId: string) {
-  const { data } = await axios.get(`/api/post/${postId}`)
-  post.value = data
+
+async function fetchPost(postId: string): Promise<void> {
+  try {
+    const { data } = await axios.get(`/api/post/${postId}`)
+    post.value = data
+  } catch (error) {
+    console.error(`Error fetching post with ID ${postId}:`, error)
+  }
 }
 
 const route = useRoute()

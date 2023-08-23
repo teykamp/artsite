ab8f08ac39036cf1cea7e234030914ace7fe8a9d
eykamt <embergameslp@gmail.com>
2023-08-22 21:42:54 -0400

    add recursion with comments and replies

--- client/src/components/CommentDisplay.vue
+++ client/src/components/CommentDisplay.vue
@@ -9,17 +9,38 @@
       'white-space': 'pre-wrap'
     }"
   >
+    <div v-if="replies.length">
+      <CommentDisplay v-for="reply in replies" :body="reply.body" :date="reply.date" />
+    </div>
   </v-card>
 </div>
 </template>
 
 <script setup lang="ts">
-import { dateDisplay } from '../composables/dateDisplay';
+import { ref } from 'vue'
+import { dateDisplay } from '../composables/dateDisplay'
+import axios from 'axios'
+import type { Comment } from '../types'
 
 const props = defineProps<{
   body: string,
-  date: string
+  date: string,
+  id?: string,
+  // replies:  number,
 }>()
+
+const replies = ref<Comment[]>([])
+const loadingReplies = ref(false)
+
+async function fetchReplies() {
+  if (replies.value.length === 0) {
+    loadingReplies.value = true
+    const { data } = await axios.get(`/api/comments/${props.id}`)
+    console.log(data)
+    replies.value = data.reverse()
+    loadingReplies.value = false
+  }
+}
 
 const { date } = dateDisplay(props.date)
 </script>

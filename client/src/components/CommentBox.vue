c8d26c1b7fa23020ab3ab6a1386bdbdc0f43797e
Thomas Eykamp <embergameslp@gmail.com>
2023-08-27 20:08:44 -0400

    add attempt custom dialog

--- client/src/components/CommentBox.vue
+++ client/src/components/CommentBox.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
     <v-textarea
-      v-model="commentArea"
-      :rules="maxCommentLength"
+      v-model="commentText"
+      :rules="commentRules"
       label="New Comment"
       auto-grow
       counter="256"
@@ -11,9 +11,20 @@
     <div class="d-flex flex-column justify-center align-center">
       <v-btn
         @click="handleCommentPost"
-        :disabled="(commentArea && commentArea.length <= maxLengthCharacters) ? false : true"
+        :disabled="!(commentText && commentText.length <= maxLengthCharacters)"
         color="primary"
       >Post</v-btn>
+      <v-btn
+        @click="resolveHandler"
+      >
+        Resolver
+      </v-btn>
+      <v-btn
+          @click="rejectHandler"
+          color="red"
+        >
+          Rejecter
+        </v-btn>
     </div>
   </div>
 </template>
@@ -28,27 +39,58 @@ const props = defineProps<{
   parentId: string,
 }>()
 
-const maxLengthCharacters: number = 256
+const maxLengthCharacters = 256
 
-const commentArea = ref("")
-const maxCommentLength = ref([v => v.length <= maxLengthCharacters || 'Character Limit Reached'])
+const rejectHandler = ref<any>(false)
+const resolveHandler = ref<any>(false)
+
+const commentText = ref("")
+const commentRules = [v => v.length <= maxLengthCharacters || 'Character Limit Reached'];
 
 function handleCommentPost() {
   props.addComment({
-    body: commentArea.value.trim(), 
+    body: commentText.value.trim(), 
     date: new Date(),
     parentId: props.parentId
   })
 
-  commentArea.value = ""
+  commentText.value = ""
 }
 
-onBeforeRouteLeave((to, from) => {
-  if (commentArea.value) {
-    const answer = window.confirm(
-      'Do you really want to leave? you have unsaved changes!'
+onBeforeRouteLeave(async () => {
+  if (!commentText.value) {
+    return true
+  }
+
+  console.log(
+    'Make A Decision'
+  )
+
+  try {
+    await makeDecision()
+    console.log(
+      'Resolved'
     )
-    if (!answer) return false
+  } catch {
+    console.log(
+      'Rejected'
+    )
+    return false
   }
 })
+
+const makeDecision = () => {
+  return new Promise((resolve, reject) => {
+
+
+    // this is the logic to display a dialog
+    //
+    //
+    //  one button will call the "reject" fn, another will call the "resolve" fn. FN
+
+
+    resolveHandler.value = resolve
+    rejectHandler.value = reject
+  })
+}
 </script>
